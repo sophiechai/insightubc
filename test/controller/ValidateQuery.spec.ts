d024fa981a96cda83c085eff2f78607215153472
Lisa <AngelLisaLam@gmail.com>
2022-02-11 13:42:55 -0800

    Add folder test for validateQuery

--- test/controller/ValidateQuery.spec.ts
+++ test/controller/ValidateQuery.spec.ts
@@ -11,9 +11,21 @@ import {
 	isSComparisonValid,
 } from "../../src/controller/ValidateQuery";
 import {expect} from "chai";
-import {InsightError} from "../../src/controller/IInsightFacade";
+import {
+	IInsightFacade,
+	InsightDatasetKind,
+	InsightError, InsightResult,
+	ResultTooLargeError
+} from "../../src/controller/IInsightFacade";
+import {getContentFromArchives} from "../TestUtil";
+import InsightFacade from "../../src/controller/InsightFacade";
+import {folderTest} from "@ubccpsc310/folder-test";
 
 let ids = ["courses", "courses2"];
+
+type Input = unknown;
+type Output = Promise<InsightResult[]>;
+type Error = "InsightError" | "ResultTooLargeError";
 
 describe("ValidateQuery", function () {
 	describe("isQueryValid", function () {
@@ -422,4 +434,43 @@ describe("ValidateQuery", function () {
 			expect(areQueryKeysValid(qkList, ids)).to.equal(true);
 		});
 	});
+});
+
+describe("Dynamic folder test for performQuery", function () {
+	this.timeout(10000);
+	let courses: string;
+	let facade: IInsightFacade;
+	before(async function () {
+		courses = getContentFromArchives("courses.zip");
+		facade = new InsightFacade();
+		await facade.addDataset("courses", courses, InsightDatasetKind.Courses);
+		await facade.addDataset("courses2", courses, InsightDatasetKind.Courses);
+	});
+
+	// Assert actual error is of expected type
+	function assertError(actual: any, expected: Error): void {
+		if (expected === "InsightError") {
+			expect(actual).to.be.an.instanceOf(InsightError);
+		} else if (expected === "ResultTooLargeError") {
+			expect(actual).to.be.an.instanceOf(ResultTooLargeError);
+		} else {
+			expect.fail("UNEXPECTED ERROR");
+		}
+	}
+
+	function assertResult(actual: any, expected: Awaited<Output>): void {
+		expect(actual).to.deep.members(expected);
+		expect(actual).to.have.length(expected.length);
+	}
+
+	folderTest<Input, Output, Error>(
+		"performQuery tests",
+		(input: Input): Output => facade.performQuery(input),
+		"./test/resources/validateQuery",
+		{
+			errorValidator: (error): error is Error => error === "InsightError" || error === "ResultTooLargeError",
+			assertOnError: assertError,
+			// assertOnResult: assertResult
+		}
+	);
 });
