2d24ea6aef3cc84a86e7558e5945c95da31d28af
Lisa <AngelLisaLam@gmail.com>
2022-03-10 19:59:02 -0800

    edit and delete comments

--- src/controller/Filter2.ts
+++ src/controller/Filter2.ts
@@ -1,9 +1,5 @@
 import {InsightError, InsightResult, ResultTooLargeError} from "./IInsightFacade";
 
-// export function sortResult(orderKey: string, resultArray: InsightResult[]) {
-// 	sort(orderKey, resultArray);
-// }
-
 export function sortResult(orderValue: any, resultArray: InsightResult[]) {
 	let dir = "UP";
 	let orderKeys: string[] = [];
@@ -15,26 +11,6 @@ export function sortResult(orderValue: any, resultArray: InsightResult[]) {
 	}
 	sort(orderKeys, resultArray, dir);
 }
-
-// function sort(orderKey: string, arr: InsightResult[]) {
-// 	let n = arr.length;
-//
-// 	// Build heap (rearrange array)
-// 	for (let i = Math.floor(n / 2) - 1; i >= 0; i--) {
-// 		heapify(arr, n, i, orderKey);
-// 	}
-//
-// 	// One by one extract an element from heap
-// 	for (let i = n - 1; i > 0; i--) {
-// 		// Move current root to end
-// 		let temp = arr[0];
-// 		arr[0] = arr[i];
-// 		arr[i] = temp;
-//
-// 		// call max heapify on the reduced heap
-// 		heapify(arr, i, 0, orderKey);
-// 	}
-// }
 
 function sort(orderKeys: string[], arr: InsightResult[], dir: string) {
 	let n = arr.length;
@@ -55,7 +31,7 @@ function sort(orderKeys: string[], arr: InsightResult[], dir: string) {
 		arr[0] = arr[i];
 		arr[i] = temp;
 
-		// call max heapify on the reduced heap
+		// call heapify on the reduced heap
 		if (dir === "UP") {
 			maxHeapify(arr, i, 0, orderKeys);
 		} else {
@@ -63,34 +39,6 @@ function sort(orderKeys: string[], arr: InsightResult[], dir: string) {
 		}
 	}
 }
-
-// To heapify a subtree rooted with node i which is
-// an index in arr[]. n is size of heap
-// function heapify(arr: InsightResult[], n: number, i: number, orderKey: string) {
-// 	let largest = i; // Initialize largest as root
-// 	let l = 2 * i + 1; // left = 2*i + 1
-// 	let r = 2 * i + 2; // right = 2*i + 2
-//
-// 	// If left child is larger than root
-// 	if (l < n && arr[l][orderKey] > arr[largest][orderKey]) {
-// 		largest = l;
-// 	}
-//
-// 	// If right child is larger than largest so far
-// 	if (r < n && arr[r][orderKey] > arr[largest][orderKey]) {
-// 		largest = r;
-// 	}
-//
-// 	// If largest is not root
-// 	if (largest !== i) {
-// 		let swap = arr[i];
-// 		arr[i] = arr[largest];
-// 		arr[largest] = swap;
-//
-// 		// Recursively heapify the affected sub-tree
-// 		heapify(arr, n, largest, orderKey);
-// 	}
-// }
 
 function maxHeapify(arr: InsightResult[], n: number, i: number, orderKeys: string[]) {
 	let largest = i; // Initialize largest as root
@@ -119,7 +67,7 @@ function maxHeapify(arr: InsightResult[], n: number, i: number, orderKeys: strin
 }
 
 function minHeapify(arr: InsightResult[], n: number, i: number, orderKeys: string[]) {
-	let smallest = i; // Initialize largest as root
+	let smallest = i; // Initialize smallest as root
 	let l = 2 * i + 1; // left = 2*i + 1
 	let r = 2 * i + 2; // right = 2*i + 2
 
@@ -133,7 +81,7 @@ function minHeapify(arr: InsightResult[], n: number, i: number, orderKeys: strin
 		smallest = r;
 	}
 
-	// If largest is not root
+	// If smallest is not root
 	if (smallest !== i) {
 		let swap = arr[i];
 		arr[i] = arr[smallest];
